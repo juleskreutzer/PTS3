c2640a10b77411c64150241cb8c9568fb3b0c375
Bart van Keersop <bart.van.keersop@gmail.com>
2015-09-21 13:20:56 +0200

    Added the Minion and Status class.

---
+++ Hack Attack/src/Minion.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Bart van Keersop
+ */
+public class Minion {
+    
+    //Fields
+    private double hp; //remaining hitpoints of the minion.
+    private double atp; //attackpower of the minion.
+    private double speed; //the speed of the unit.
+    private Status[] statuses; //the statuses of the unit.
+    
+    
+}
