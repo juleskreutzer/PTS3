54703643ece096e8c10f6a5584a379e296d8d2d4
juleskreutzer <j.kreutzer@student.fontys.nl>
2015-09-21 13:22:52 +0200

    added some classes

---
+++ Hack Attack/src/hack/attack/Defense.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hack.attack;
+
+import java.awt.Point;
+
+/**
+ *
+ * @author juleskreutzer
+ */
+public class Defense {
+    
+    private Double cost;
+    private Point xPos;
+    private Point yPos;
+    private Defense_Type type;
+    
+    
+}
