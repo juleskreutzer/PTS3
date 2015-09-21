da298b2c73159a20e259057c7b983b86ba79e40d
Bart van Keersop <bart.van.keersop@gmail.com>
2015-09-21 14:05:38 +0200

    Minion now has fields, constructor and GET methods.

---
+++ Hack Attack/src/hack/attack/Status.java
@@ -0,0 +1,20 @@
+package hack.attack;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Bart van Keersop
+ */
+import java.time;
+public class Status {
+    
+    private String name;
+    private DateTime tiem;
+    
+    
+}
