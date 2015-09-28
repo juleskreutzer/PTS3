a0df530f7576e38ab4f67a1e80939addb81446d1
juleskreutzer <j.kreutzer@student.fontys.nl>
2015-09-28 16:35:35 +0200

    Added stuff to minionEffect and Player, PLAYER ISN'T FINISHED!

--- Hack Attack/src/hack/attack/Player.java
+++ Hack Attack/src/hack/attack/Player.java
@@ -5,12 +5,40 @@
  */
 package hack.attack;
 
+import java.awt.Point;
+import java.util.List;
+
 /**
  *
  * @author juleskreutzer
  */
 public class Player {
-
+    
+    private double health;
+    private String name;
+    private double bitCoints;
+    private Point[] baseLocation;
+    private List<Module> modules;
+    
+    /**
+     * Construct the player instance based on the data from the DATA-class
+     * @param name Name of the player
+     */
+    public Player(String name)
+    {
+        
+    }
+    
+    /**
+     * Build a softwareinjector and return the spells that became available
+     * @return List of Spells that became available
+     */
+    public List<Spell> buildSoftwareInjector()
+    {
+        return null;
+    }
+    
+    
     void receiveDamage(double damage) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
