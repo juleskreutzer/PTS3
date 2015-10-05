389a1a2e5bd2e371a8a44ec34f8552ec84223396
juleskreutzer <j.kreutzer@student.fontys.nl>
2015-10-05 19:38:04 +0200

    Removed default constructors in the fields

--- Hack Attack/src/hack/attack/Data.java
+++ Hack Attack/src/hack/attack/Data.java
@@ -21,21 +21,61 @@ import org.json.*;
  */
 public class Data {
     
-    public static MinionTemplate DEFAULT_BYTE = new MinionTemplate(MinionType.b,1,1,1,false,1);
-    public static MinionTemplate DEFAULT_KILOBYTE = new MinionTemplate(MinionType.kb,2,2,2,false,2);
-    public static MinionTemplate DEFAULT_MEGABYTE = new MinionTemplate(MinionType.mb,3,3,3,true,3);
-    public static MinionTemplate DEFAULT_GIGABYTE = new MinionTemplate(MinionType.gb,4,4,4,false,4);
-    public static MinionTemplate DEFAULT_TERABYTE = new MinionTemplate(MinionType.tb,5,5,5,true,5);
-    public static MinionTemplate DEFAULT_PETABYTE = new MinionTemplate(MinionType.pb,5,5,5,true,5);
-
-
+    public static MinionTemplate DEFAULT_BYTE;
+    public static MinionTemplate DEFAULT_KILOBYTE;
+    public static MinionTemplate DEFAULT_MEGABYTE;
+    public static MinionTemplate DEFAULT_GIGABYTE;
+    public static MinionTemplate DEFAULT_TERABYTE;
+    public static MinionTemplate DEFAULT_PETABYTE;
     
     private static String urlMinion = "http://api.nujules.nl/minion";
+    private static String urlSpell = "http://api.nujules.nl/spell";
+    private static String urlModule = "http://api.nujules.nl/module";
     
     public Data() throws IOException, InvalidMinionTypeException
     {
+        /**
+         * First we create the JSONArray object by requesting the data from the HackAttack API.
+         * Then we call the create method for the objects we want te create which will parse the JSONArray.
+         */
         JSONArray minions = sendGet(urlMinion);
+        JSONArray spells = sendGet(urlSpell);
+        JSONArray modules = sendGet(urlModule);
         
+        createMinions(minions);
+    }
+    
+    private JSONArray sendGet(String url) throws IOException
+    {
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A"); // Do as if you're using Chrome 41 on Windows 7.
+        con.setRequestMethod("GET");
+        con.setRequestProperty("X_AUTH_TOKEN", "test"); // API checks access based on x-auth-token header
+        
+        int responseCode = con.getResponseCode();
+        if(responseCode == HttpURLConnection.HTTP_OK)
+        {
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    con.getInputStream()));
+            String inputLine;
+            StringBuilder sb = new StringBuilder();
+            String response;
+ 
+            while ((inputLine = in.readLine()) != null) {
+                sb.append(inputLine + "\n");
+            }
+            in.close();
+            response = sb.toString();
+ 
+            // print result
+            return new JSONArray(response);
+        }
+        return null;
+    }
+    
+    private void createMinions(JSONArray minions) throws InvalidMinionTypeException
+    {
         for(int i = 0; i < minions.length(); i++)
         {
             JSONObject obj = minions.getJSONObject(i);
@@ -83,41 +123,23 @@ public class Data {
                     throw new InvalidMinionTypeException();
             }
         }
-        
-        System.out.print(DEFAULT_BYTE.getDamage());
-        
-            
-        
-        
     }
     
-    private JSONArray sendGet(String url) throws IOException
+    private void createSpells(JSONArray spells)
     {
-        URL obj = new URL(url);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-        con.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"); // Do as if you're using Chrome 41 on Windows 7.
-        con.setRequestMethod("GET");
-        con.setRequestProperty("X_AUTH_TOKEN", "test"); // API checks access based on x-auth-token header
-        
-        int responseCode = con.getResponseCode();
-        if(responseCode == HttpURLConnection.HTTP_OK)
+        for(int i = 0; i < spells.length(); i++)
         {
-            BufferedReader in = new BufferedReader(new InputStreamReader(
-                    con.getInputStream()));
-            String inputLine;
-            StringBuilder sb = new StringBuilder();
-            String response;
- 
-            while ((inputLine = in.readLine()) != null) {
-                sb.append(inputLine + "\n");
-            }
-            in.close();
-            response = sb.toString();
- 
-            // print result
-            return new JSONArray(response);
+            
         }
-        return null;
+        
     }
     
+    private void createModules(JSONArray modules)
+    {
+        for(int i = 0; i < modules.length(); i++)
+        {
+            
+        }
+        
+    }
 }
