3021b9c62411bb83500537e19ed34dd96b34ea5a
Bart van Keersop <bart.van.keersop@gmail.com>
2015-11-02 11:59:26 +0100

    RoadTest Implemented

---
+++ HackAttackFX/test/hackattackfx/PathTest.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hackattackfx;
+
+import hackattackfx.exceptions.FloatingPathException;
+import java.awt.Point;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Bart van Keersop
+ */
+public class PathTest {
+
+    public PathTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getStart method, of class Path.
+     */
+    @Test
+    public void testGetStart() {
+
+        
+    }
+
+    /**
+     * Test of getEnd method, of class Path.
+     */
+    @Test
+    public void testGetEnd() {
+        
+    }
+
+    /**
+     * Test of getLength method, of class Path.
+     */
+    @Test
+    public void testGetLength() {
+      
+    }
+
+    /**
+     * Test of getDirection method, of class Path.
+     */
+    @Test
+    public void testGetDirection() {
+     
+    }
+    
+}
